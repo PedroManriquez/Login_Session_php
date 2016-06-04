47c9f96a7641f7803f16781b07c51c003674662c
PedroManriquez <ptrmanriquez@gmail.com>
2016-06-04 00:43:08 -0400

    Add files via upload

---
+++ js/canciones.js
@@ -0,0 +1,316 @@
+var songs= [
+  {
+    "songname": "Immortality - Live",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/f7031150bc119d9e7a7ae87f2c922d5f8637b9b2"
+  },
+  {
+    "songname": "Help Help",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/1ec48d11a57a9fb61cf69d74cb62add128020602"
+  },
+  {
+    "songname": "Blood",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/a61e331a87f008cb2720d753aa3c14f700e68a6e"
+  },
+  {
+    "songname": "Sometimes",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/36706484848fa6c2d28e85209f8e4399a8d9cb7c"
+  },
+  {
+    "songname": "The End",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/f7031150bc119d9e7a7ae87f2c922d5f8637b9b2"
+  },
+  {
+    "songname": "Off He Goes",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/36706484848fa6c2d28e85209f8e4399a8d9cb7c"
+  },
+  {
+    "songname": "Stupidmop - Remastered",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/f5346f2524b4db4af9d03e581c21adc27f2c7f89"
+  },
+  {
+    "songname": "Just Breathe",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/f7031150bc119d9e7a7ae87f2c922d5f8637b9b2"
+  },
+  {
+    "songname": "Corduroy",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/a61e331a87f008cb2720d753aa3c14f700e68a6e"
+  },
+  {
+    "songname": "Johnny Guitar",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/f7031150bc119d9e7a7ae87f2c922d5f8637b9b2"
+  },
+  {
+    "songname": "Light Years",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/520b0e396250a665aef0d45ae75662f7a31107d8"
+  },
+  {
+    "songname": "Oceans",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/425e1fef6d70a48a103dfd168a556b7f72f72865"
+  },
+  {
+    "songname": "Low Light",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/0eb58fa9704eebd901dc4537937f47f4b66e9578"
+  },
+  {
+    "songname": "Come Back",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/40d9b256cf76339b9f31fb36adff36b726b19b36"
+  },
+  {
+    "songname": "Tremor Christ - Remastered",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/f5346f2524b4db4af9d03e581c21adc27f2c7f89"
+  },
+  {
+    "songname": "Johnny Guitar",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/f7031150bc119d9e7a7ae87f2c922d5f8637b9b2"
+  },
+  {
+    "songname": "Alive - Brendan O'Brien Mix",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/9480b5c6a25561c85a805733c6e6acc993cf78c4"
+  },
+  {
+    "songname": "Garden - Brendan O'Brien Mix",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/9480b5c6a25561c85a805733c6e6acc993cf78c4"
+  },
+  {
+    "songname": "Whipping - Remastered",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/f5346f2524b4db4af9d03e581c21adc27f2c7f89"
+  },
+  {
+    "songname": "Faithful",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/0eb58fa9704eebd901dc4537937f47f4b66e9578"
+  },
+  {
+    "songname": "Do The Evolution",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/0eb58fa9704eebd901dc4537937f47f4b66e9578"
+  },
+  {
+    "songname": "Wasted Reprise",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/40d9b256cf76339b9f31fb36adff36b726b19b36"
+  },
+  {
+    "songname": "W.M.A. (Remastered)",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/4ce3b5d43c7eba8fdbd63f4238ef84a0311b2779"
+  },
+  {
+    "songname": "The End",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/f7031150bc119d9e7a7ae87f2c922d5f8637b9b2"
+  },
+  {
+    "songname": "Love Boat Captain",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/1ec48d11a57a9fb61cf69d74cb62add128020602"
+  },
+  {
+    "songname": "I'm Open",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/36706484848fa6c2d28e85209f8e4399a8d9cb7c"
+  },
+  {
+    "songname": "Aya Davanita - Remastered",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/f5346f2524b4db4af9d03e581c21adc27f2c7f89"
+  },
+  {
+    "songname": "Alive",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/425e1fef6d70a48a103dfd168a556b7f72f72865"
+  },
+  {
+    "songname": "Army Reserve",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/40d9b256cf76339b9f31fb36adff36b726b19b36"
+  },
+  {
+    "songname": "Dissident (Remastered)",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/4ce3b5d43c7eba8fdbd63f4238ef84a0311b2779"
+  },
+  {
+    "songname": "Severed Hand",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/40d9b256cf76339b9f31fb36adff36b726b19b36"
+  },
+  {
+    "songname": "Unthought Known",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/f7031150bc119d9e7a7ae87f2c922d5f8637b9b2"
+  },
+  {
+    "songname": "Better Man - Live",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/f7031150bc119d9e7a7ae87f2c922d5f8637b9b2"
+  },
+  {
+    "songname": "Jeremy - Brendan O'Brien Mix",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/9480b5c6a25561c85a805733c6e6acc993cf78c4"
+  },
+  {
+    "songname": "Grievance",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/520b0e396250a665aef0d45ae75662f7a31107d8"
+  },
+  {
+    "songname": "Bugs - Remastered",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/f5346f2524b4db4af9d03e581c21adc27f2c7f89"
+  },
+  {
+    "songname": "Just Breathe",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/f7031150bc119d9e7a7ae87f2c922d5f8637b9b2"
+  },
+  {
+    "songname": "Who You Are",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/36706484848fa6c2d28e85209f8e4399a8d9cb7c"
+  },
+  {
+    "songname": "Better Man - Remastered",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/f5346f2524b4db4af9d03e581c21adc27f2c7f89"
+  },
+  {
+    "songname": "Can't Keep",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/1ec48d11a57a9fb61cf69d74cb62add128020602"
+  },
+  {
+    "songname": "Bu$hleaguer",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/1ec48d11a57a9fb61cf69d74cb62add128020602"
+  },
+  {
+    "songname": "Push Me, Pull Me",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/0eb58fa9704eebd901dc4537937f47f4b66e9578"
+  },
+  {
+    "songname": "Evacuation",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/520b0e396250a665aef0d45ae75662f7a31107d8"
+  },
+  {
+    "songname": "Animal (Remastered)",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/4ce3b5d43c7eba8fdbd63f4238ef84a0311b2779"
+  },
+  {
+    "songname": "1/2 Full",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/1ec48d11a57a9fb61cf69d74cb62add128020602"
+  },
+  {
+    "songname": "Pendulum",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/b4e9dcd21991b68ea11d4dc848f85c95a80562f6"
+  },
+  {
+    "songname": "The Fixer",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/f7031150bc119d9e7a7ae87f2c922d5f8637b9b2"
+  },
+  {
+    "songname": "Breakerfall",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/520b0e396250a665aef0d45ae75662f7a31107d8"
+  },
+  {
+    "songname": "In My Tree",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/36706484848fa6c2d28e85209f8e4399a8d9cb7c"
+  },
+  {
+    "songname": "Tremor Christ",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/a61e331a87f008cb2720d753aa3c14f700e68a6e"
+  },
+  {
+    "songname": "Yellow Moon",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/b4e9dcd21991b68ea11d4dc848f85c95a80562f6"
+  },
+  {
+    "songname": "Why Go",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/425e1fef6d70a48a103dfd168a556b7f72f72865"
+  },
+  {
+    "songname": "Last Exit - Live",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/f7031150bc119d9e7a7ae87f2c922d5f8637b9b2"
+  },
+  {
+    "songname": "Porch - Brendan O'Brien Mix",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/9480b5c6a25561c85a805733c6e6acc993cf78c4"
+  },
+  {
+    "songname": "Indifference (Remastered)",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/4ce3b5d43c7eba8fdbd63f4238ef84a0311b2779"
+  },
+  {
+    "songname": "Got Some",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/f7031150bc119d9e7a7ae87f2c922d5f8637b9b2"
+  },
+  {
+    "songname": "Around The Bend",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/36706484848fa6c2d28e85209f8e4399a8d9cb7c"
+  },
+  {
+    "songname": "Rats (Remastered)",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/4ce3b5d43c7eba8fdbd63f4238ef84a0311b2779"
+  },
+  {
+    "songname": "Last Exit - Remastered",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/f5346f2524b4db4af9d03e581c21adc27f2c7f89"
+  },
+  {
+    "songname": "Evil Little Goat - Rehearsal studio outtake",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/9480b5c6a25561c85a805733c6e6acc993cf78c4"
+  },
+  {
+    "songname": "Sirens",
+    "artist": "Pearl Jam",
+    "img": "https://i.scdn.co/image/b4e9dcd21991b68ea11d4dc848f85c95a80562f6"
+  }
+];
+
+$(document).ready(function() {
+
+  for(var i=0; i<songs.length;i++){
+    $('.container').append(
+        '<br><div class="row" ><div class="row claseAlgo"><div class="col-xs-2 columna"><img src="'+songs[i].img+'" alt="" ></div>  '+'      <div class="col-xs-10 columna"><h1>'+songs[i].songname+'</h1><p>'+songs[i].artist+'</p></div></div></div>'
+    );
+  }
+});
